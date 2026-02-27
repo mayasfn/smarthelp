b7b8ce7c3b7894987b898f44cf45dfc9f8b3715a
Camelia Mazouz <145675762+mica1505@users.noreply.github.com>
2026-02-27 16:19:38 +0100

    oups api key

--- backend/llm/model.py
+++ backend/llm/model.py
@@ -1,10 +1,11 @@
 from langchain_mistralai import ChatMistralAI
 import dotenv
+import os
 dotenv.load_dotenv()
 
 
 llm = ChatMistralAI(
     model="mistral-small-latest",
-    mistral_api_key="uYjtS4EklNWuYR87xB0KLXNiIqxcT4mx",
+    mistral_api_key=os.getenv("OPENAI_API_KEY"),
     temperature=1,
 )
