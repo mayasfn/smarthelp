f5afbd6dfb14f006d966cc6ed04f774b1f9d8a8b
tgayet <tgayet@deezer.com>
2026-03-01 16:10:17 +0100

    fix cortex search

--- backend/db/cortex_search_services/RESOLVED_TICKETS_SEARCH_SERVICE.sql
+++ backend/db/cortex_search_services/RESOLVED_TICKETS_SEARCH_SERVICE.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE CORTEX SEARCH SERVICE RESOLVED_TICKETS_SEARCH
-ON semantic_text
+ON BODY_ANSWER
 ATTRIBUTES
     SUBJECT,
     TYPE,
     QUEUE,
     PRIORITY,
     LANGUAGE,
-    VERSION
+    VERSION,
+    BODY_ANSWER
 WAREHOUSE = COMPUTE_WH
 TARGET_LAG = '1 hour'
 EMBEDDING_MODEL = 'snowflake-arctic-embed-m-v1.5'
@@ -29,6 +30,6 @@ SELECT
             TAG_1, TAG_2, TAG_3, TAG_4,
             TAG_5, TAG_6, TAG_7, TAG_8
         )
-    ) AS semantic_text
+    ) AS BODY_ANSWER
 
 FROM RESOLVED_TICKETS;
